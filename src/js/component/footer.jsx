d895cfc317b76da7a1501a820fa63edadfa6b9f0
YahairaCM <63628432+Yahaira326@users.noreply.github.com>
2023-08-18 18:34:45 +0000

    up

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../../styles/index.css";
+
+function Footer(props) {
+    return (
+        <div className="footer">
+            <p>Copyright © Lesco! 2023</p>
+        </div>
+    )
+}
+export default Footer; 
