58052c420e79ff635ed510eeb95e0446c82c92ab
YahairaCM <63628432+Yahaira326@users.noreply.github.com>
2023-08-18 18:41:09 +0000

    final

---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../../styles/index.css";
+
+function Jumbotron(props) {
+    return (
+        <div className="jumbotron col-12 bg-light p-3 rounded">
+        <h1 className="display-4"> Cursos de LESCO CR!</h1>
+        <p> Te invitamos a empezar tu camino de aprendizaje de la LESCO y de la Cultura Sorda con nosotros. </p>
+        <p className="lead">
+        <a className="btn btn-primary btn-lg" href="#" role="button"> action!</a>
+        </p>
+    </div>
+    )
+}
+export default Jumbotron; 
+
