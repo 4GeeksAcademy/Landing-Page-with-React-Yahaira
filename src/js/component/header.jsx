c73d34afc0cc773c6d7f3e3cec667a0f592e4c21
YahairaCM <63628432+Yahaira326@users.noreply.github.com>
2023-08-18 18:38:08 +0000

    up

--- src/js/component/header.jsx
+++ src/js/component/header.jsx
@@ -1,13 +1,35 @@
-//import react into the bundle
 import React from "react";
-import ReactDOM from "react-dom";
+import "../../styles/index.css";
 
-// include your styles into the webpack bundle
-import "../styles/index.css";
+function Header(props) {
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            <div className="container-fluid">
+            <a className="navbar-brand">Start </a>
+            
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="menu collapse navbar-collapse" id="navbarsExample03">
 
-//import your own components
-import Home from "./component/home.jsx";
+            <ul className="navbar-nav ms-auto mb-2 mb-sm-0 navbar-dark bg-dark">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">¿Quiénes Somos?</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Cursos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">Contacto</a>
+                </li>
+            </ul>
+            </div>
+            </div>
+        </nav>
+    )
+}
+export default Header; 
 
-
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
